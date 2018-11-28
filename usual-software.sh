2972d601c0a761563a3cafaab514b7ccce85382c
移影残风 <yiyingcanfeng@qq.com>
2018-11-28 10:00:20 +0800

    增加软件

--- usual-software.sh
+++ usual-software.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum install -y vim epel-release python34-pip screen lrzsz supervisor iotop ncdu pv htop net-tools sl iftop
+yum install -y bash-completion vim epel-release python34-pip screen lrzsz supervisor iotop ncdu pv htop net-tools sl iftop
