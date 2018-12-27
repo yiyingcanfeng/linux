ece933af8810b6fbba10035e6b32f4a723b4da30
移影残风 <yiyingcanfeng@qq.com>
2018-12-27 10:59:02 +0800

    修改了一些东西

--- usual-software.sh
+++ usual-software.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum install -y bash-completion vim epel-release yum-utils python34-pip screen lrzsz supervisor iotop ncdu pv htop net-tools sl iftop
+yum install -y bash-completion wget vim epel-release yum-utils python34-pip screen lrzsz supervisor iotop ncdu pv htop net-tools sl iftop lynx links
