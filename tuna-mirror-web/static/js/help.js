4c86be66d737873bef970e1fc33d0d43ff0fee01
移影残风 <yiyingcanfeng@qq.com>
2018-12-27 13:32:51 +0800

    TUNA mirrors 主页前端源码

---
+++ tuna-mirror-web/static/js/help.js
@@ -0,0 +1,33 @@
+"use strict";
+
+$(document).ready(function () {
+	$("#help-content").find('table').addClass("table table-bordered table-striped");
+
+	var update_apt_file = function update_apt_file(ev) {
+		var sel = $(ev.target),
+		    os_name = sel.find("option:selected").data('os'),
+		    release_name = sel.find("option:selected").data('release'),
+		    opt = sel.find('option:selected').data('opt'),
+		    tmpl_selector = sel.data("template"),
+		    target_selector = sel.data("target"),
+		    apt_template = $.trim($(tmpl_selector).text()),
+		    tmpl_data = $.extend({}, {
+			os_name: os_name,
+			release_name: release_name
+		}, opt),
+		    apt_content = Mark.up(apt_template, tmpl_data);
+		$(target_selector).html(apt_content);
+	};
+
+	$("select.release-select").on('change', update_apt_file);
+	$("select.release-select").each(function (i, e) {
+		$(e).trigger('change');
+	});
+
+	$('#help-select').on('change', function (ev) {
+		var help_url = $(ev.target).find("option:selected").data('help-url');
+		window.location = window.location.protocol + "//" + window.location.host + help_url;
+	});
+});
+
+// vim: ts=2 sts=2 sw=2 noexpandtab
---
+++ tuna-mirror-web/static/js/status.js
@@ -0,0 +1,52 @@
+'use strict';
+
+$(document).ready(function () {
+	var mir_tmpl = $("#template").text();
+	function readableFileSize(size) {
+		var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		var i = 0;
+		while (size >= 1024) {
+			size /= 1024;
+			++i;
+		}
+		return size.toFixed(1) + ' ' + units[i];
+	}
+	$.get("/static/status/disk.json", function (d) {
+		var used_percent = Math.round(d.used_kb * 100 / d.total_kb);
+		$('#disk-usage-bar').attr("aria-valuenow", used_percent).css("width", used_percent + "%").html("<strong>" + readableFileSize(d.used_kb * 1024) + " / " + readableFileSize(d.total_kb * 1024) + "</strong>");
+	});
+
+	window.refreshMirrorList = function () {
+		$.getJSON("/static/tunasync.json", function (status_data) {
+			var mirrors = [],
+			    mir_data = status_data;
+
+			mir_data.sort(function (a, b) {
+				return a.name < b.name ? -1 : 1;
+			});
+
+			for (var k in mir_data) {
+				var d = mir_data[k];
+				if (d.is_master === undefined) {
+					d.is_master = true;
+				}
+				// Strip the second component of last_update
+				if (d.last_update_ts) {
+					var date = new Date(d.last_update_ts * 1000);
+					if (date.getFullYear() > 2000) {
+						d.last_update = ('000' + date.getFullYear()).substr(-4) + '-' + ('0' + (date.getMonth() + 1)).substr(-2) + '-' + ('0' + date.getDate()).substr(-2) + (' ' + ('0' + date.getHours()).substr(-2) + ':' + ('0' + date.getMinutes()).substr(-2));
+					} else {
+						d.last_update = "0000-00-00 00:00";
+					}
+				} else {
+					d.last_update = d.last_update.replace(/(\d\d:\d\d):\d\d(\s\+\d\d\d\d)?/, '$1');
+				}
+				mirrors.push(d);
+			}
+			var result = Mark.up(mir_tmpl, { mirrors: mirrors });
+			$('#mirror-list').html(result);
+		});
+		setTimeout(refreshMirrorList, 10000);
+	};
+	refreshMirrorList();
+});
