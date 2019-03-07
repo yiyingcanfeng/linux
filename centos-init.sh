1d57ae0246451b474638737f4dd8e4717a139827
移影残风 <yiyingcanfeng@qq.com>
2019-03-07 16:22:35 +0800

    脚本优化

--- centos-init.sh
+++ centos-init.sh
@@ -3,24 +3,24 @@
 # curl https://yiyingcanfeng.github.io/centos-init.sh | bash -s python php nodejs cmd_game jdk mysql mongodb docker
 
 function system_config() {
-# 修改主机名
-#hostnamectl set-hostname aaa
-# 禁用selinux
-sed -i 's/SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
-setenforce 0
-# 修改开机引导等待时间
-sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-# 请根据具体情况来决定是否关闭防火墙
-systemctl  stop firewalld
-systemctl  disable  firewalld
+    # 修改主机名
+    #hostnamectl set-hostname aaa
+    # 禁用selinux
+    sed -i 's/SELINUX=.*/SELINUX=permissive/g' /etc/selinux/config
+    setenforce 0
+    # 修改开机引导等待时间
+    sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+    # 请根据具体情况来决定是否关闭防火墙
+    systemctl  stop firewalld
+    systemctl  disable  firewalld
 }
 
 function config_mirror_and_update() {
 #更换yum源
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-#curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-cat > /etc/yum.repos.d/CentOS-Base.repo <<- "EOF"
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+    #curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+    cat > /etc/yum.repos.d/CentOS-Base.repo <<- "EOF"
 [base]
 name=CentOS-$releasever - Base
 baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos/$releasever/os/$basearch/
@@ -54,15 +54,18 @@ enabled=0
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 EOF
-yum makecache
+    yum makecache
+    #同步时间
+    yum install -y ntpdate
+    ntpdate time.windows.com
 
-#配置EPEL源
-#EPEL (Extra Packages for Enterprise Linux) 是由 Fedora Special Interest Group 为企业 Linux 创建、维护和管理的一个高质量附加包集合，适用于但不仅限于 Red Hat Enterprise Linux (RHEL), CentOS, Scientific Linux (SL), Oracle Linux (OL)
-yum install -y epel-release
-mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
-#curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-cat > /etc/yum.repos.d/epel.repo <<- "EOF"
+    #配置EPEL源
+    #EPEL (Extra Packages for Enterprise Linux) 是由 Fedora Special Interest Group 为企业 Linux 创建、维护和管理的一个高质量附加包集合，适用于但不仅限于 Red Hat Enterprise Linux (RHEL), CentOS, Scientific Linux (SL), Oracle Linux (OL)
+    yum install -y epel-release
+    mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
+    mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
+    #curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+    cat > /etc/yum.repos.d/epel.repo <<- "EOF"
 [epel]
 name=Extra Packages for Enterprise Linux 7 - $basearch
 baseurl=https://mirrors.tuna.tsinghua.edu.cn/epel/7/$basearch
@@ -93,7 +96,7 @@ EOF
 
 #配置ius源
 #IUS只为RHEL和CentOS这两个发行版提供较新版本的rpm包。如果在os或epel找不到某个软件的新版rpm，软件官方又只提供源代码包的时候，可以来ius源中找，几乎都能找到。比如，python3.6(包括对应版本的pip，epel源里有python3.6但没有对应版本的pip),php7.2,redis5等等
-cat > /etc/yum.repos.d/ius.repo <<- "EOF"
+    cat > /etc/yum.repos.d/ius.repo <<- "EOF"
 [ius]
 name=ius
 baseurl=https://mirrors.tuna.tsinghua.edu.cn/ius/stable/CentOS/7/$basearch
@@ -102,13 +105,13 @@ gpgcheck=0
 enabled=1
 
 EOF
-yum makecache
-yum update -y
+    yum makecache
+    yum update -y
 }
 
 function install_usual_software() {
 #一些实用工具,这些大部分在EPEL源里
-yum install -y bash-completion git wget vim nano yum-utils unar screen lrzsz supervisor iotop iftop jnettop mytop apachetop atop htop ncdu nmap pv net-tools sl lynx links crudini the_silver_searcher tig cloc nload w3m axel tmux mc glances multitail redis5
+    yum install -y bash-completion git wget vim nano yum-utils unar screen lrzsz supervisor iotop iftop jnettop mytop apachetop atop htop ncdu nmap pv net-tools sl lynx links crudini the_silver_searcher tig cloc nload w3m axel tmux mc glances multitail redis5
 
 }
 
@@ -118,7 +121,7 @@ yum install python36u-pip -y
 # 使用国内pypi源,使用阿里云的源
 # 备选：http://pypi.douban.com/simple/  https://pypi.tuna.tsinghua.edu.cn/simple/  https://mirrors.aliyun.com/pypi/simple/
 mkdir -p ~/.pip
-cat > ~/.pip/pip.conf <<- "EOF"
+    cat > ~/.pip/pip.conf <<- "EOF"
 [global]
 index-url = https://mirrors.aliyun.com/pypi/simple/
 
@@ -126,20 +129,20 @@ index-url = https://mirrors.aliyun.com/pypi/simple/
 trusted-host=mirrors.aliyun.com
 
 EOF
-pip3.6 install --upgrade pip
-# 一些基于python的实用或者有意思的工具
-pip3.6 install cheat mycli icdiff you-get lolcat youtube-dl
+    pip3.6 install --upgrade pip
+    # 一些基于python的实用或者有意思的工具
+    pip3.6 install cheat mycli icdiff you-get lolcat youtube-dl
 
 }
 
 function install_php() {
-yum install php72u* nginx -y
+    yum install php72u* nginx -y
 }
 
 function install_nodejs_and_config() {
 #配置nodejs的yum源，安装 nodejs (epel源里有nodejs，但版本比较老),使用清华大学的源
-yum install https://mirrors.tuna.tsinghua.edu.cn/nodesource/rpm_10.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm -y
-cat > /etc/yum.repos.d/nodesource-el7.repo <<- "EOF"
+    yum install https://mirrors.tuna.tsinghua.edu.cn/nodesource/rpm_10.x/el/7/x86_64/nodesource-release-el7-1.noarch.rpm -y
+    cat > /etc/yum.repos.d/nodesource-el7.repo <<- "EOF"
 [nodesource]
 name=Node.js Packages for Enterprise Linux 7 - $basearch
 baseurl=https://mirrors.tuna.tsinghua.edu.cn/nodesource/rpm_10.x/el/7/$basearch
@@ -153,34 +156,34 @@ enabled=0
 gpgcheck=1
 
 EOF
-yum makecache
-yum install nodejs -y
-# 更换国内npm源
-npm config set registry https://mirrors.huaweicloud.com/repository/npm/
-# 备选：npm config set registry https://mirrors.huaweicloud.com/repository/npm/
-# npm config set registry https://registry.npm.taobao.org/
-npm cache clean -f
-# 一些基于nodejs的实用或者有意思的工具
-npm install --global n npm get-port-cli hasha-cli http-server
+    yum makecache
+    yum install nodejs -y
+    # 更换国内npm源
+    npm config set registry https://mirrors.huaweicloud.com/repository/npm/
+    # 备选：npm config set registry https://mirrors.huaweicloud.com/repository/npm/
+    # npm config set registry https://registry.npm.taobao.org/
+    npm cache clean -f
+    # 一些基于nodejs的实用或者有意思的工具
+    npm install --global n npm get-port-cli hasha-cli http-server
 
 }
 
 #命令行小游戏哦
 function install_cmd_game() {
-# 2048游戏的shell实现
-curl https://raw.githubusercontent.com/mydzor/bash2048/master/bash2048.sh -o 2048.sh && chmod 755 2048.sh
-# 扫雷游戏的shell实现
-curl https://raw.githubusercontent.com/feherke/Bash-script/master/minesweeper/minesweeper.sh -o minesweeper.sh && chmod 755 minesweeper.sh
-# 命令行下的俄罗斯方块游戏
-git clone https://github.com/uuner/sedtris.git
-chmod 755 sedtris/*
-# ./sedtris/sedtris.sh
+    # 2048游戏的shell实现
+    curl https://raw.githubusercontent.com/mydzor/bash2048/master/bash2048.sh -o 2048.sh && chmod 755 2048.sh
+    # 扫雷游戏的shell实现
+    curl https://raw.githubusercontent.com/feherke/Bash-script/master/minesweeper/minesweeper.sh -o minesweeper.sh && chmod 755 minesweeper.sh
+    # 命令行下的俄罗斯方块游戏
+    git clone https://github.com/uuner/sedtris.git
+    chmod 755 sedtris/*
+    # ./sedtris/sedtris.sh
 }
 
 #安装jdk和tomcat
 function install_jdk_and_tomcat() {
 #安装openjdk
-yum install java-1.8.0-openjdk-devel.x86_64 -y
+    yum install java-1.8.0-openjdk-devel.x86_64 -y
 
 #或者 oraclejdk
 # wget https://mirrors.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
@@ -195,10 +198,10 @@ yum install java-1.8.0-openjdk-devel.x86_64 -y
 
 #安装tomcat
 #https://tomcat.apache.org/download-90.cgi 注：请随时关注官网的最新版本，新版本发布后旧版本的链接会失效！
-cd /usr
-wget https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
-tar -zxf apache-tomcat-9.0.16.tar.gz
-cat > /usr/lib/systemd/system/tomcat.service <<- "EOF"
+    cd /usr
+    wget https://mirrors.tuna.tsinghua.edu.cn/apache/tomcat/tomcat-9/v9.0.16/bin/apache-tomcat-9.0.16.tar.gz
+    tar -zxf apache-tomcat-9.0.16.tar.gz
+    cat > /usr/lib/systemd/system/tomcat.service <<- "EOF"
 [Unit]
 Description=Tomcat9.0.16
 After=syslog.target network.target remote-fs.target nss-lookup.target
@@ -217,14 +220,14 @@ PrivateTmp=true
 WantedBy=multi-user.target
 
 EOF
-systemctl daemon-reload
-systemctl start tomcat
+    systemctl daemon-reload
+    systemctl start tomcat
 
 }
 
 #安装mysql5.7 http://mirrors.tuna.tsinghua.edu.cn/mysql,使用清华大学的源
 function install_mysql_and_config() {
-cat > /etc/yum.repos.d/mysql-community.repo <<- "EOF"
+    cat > /etc/yum.repos.d/mysql-community.repo <<- "EOF"
 [mysql-connectors-community]
 name=MySQL Connectors Community
 baseurl=http://mirrors.tuna.tsinghua.edu.cn/mysql/yum/mysql-connectors-community-el7
@@ -255,34 +258,34 @@ gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
 
 EOF
 
-yum install mysql-community-server -y
-#mysql配置
-if [[ "${MYSQL_PASSWORD}" == "" ]];then
-MYSQL_PASSWORD=1111 #root用户密码
-fi
-systemctl start mysqld
-systemctl enable mysqld
-passlog=$(grep 'temporary password'  /var/log/mysqld.log)
-pass=${passlog:${#passlog}-12:${#passlog}}
-mysql -uroot -p"${pass}" -e"alter user root@localhost identified by 'QQQqqq111...' " --connect-expired-password
-pass=QQQqqq111...
-mysql -uroot -p"${pass}" -e"set global validate_password_policy=0;" --connect-expired-password
-mysql -uroot -p"${pass}" -e"set global validate_password_length=4;" --connect-expired-password
-mysql -uroot -p"${pass}" -e"set global validate_password_mixed_case_count=0;" --connect-expired-password
-mysql -uroot -p"${pass}" -e"set global validate_password_number_count=0;" --connect-expired-password
-#echo 'enter your mysql password'
-#read password
-mysql -uroot -p"${pass}" -e"set password=password('${MYSQL_PASSWORD}');" --connect-expired-password
-mysql -uroot -p"${MYSQL_PASSWORD}" -e"update mysql.user set host='%' where user='root';" --connect-expired-password
-mysql -uroot -p"${MYSQL_PASSWORD}" -e"flush privileges;" --connect-expired-password
+    yum install mysql-community-server -y
+    #mysql配置
+    if [[ "${MYSQL_PASSWORD}" == "" ]];then
+    MYSQL_PASSWORD=1111 #root用户密码
+    fi
+    systemctl start mysqld
+    systemctl enable mysqld
+    passlog=$(grep 'temporary password'  /var/log/mysqld.log)
+    pass=${passlog:${#passlog}-12:${#passlog}}
+    mysql -uroot -p"${pass}" -e"alter user root@localhost identified by 'QQQqqq111...' " --connect-expired-password
+    pass=QQQqqq111...
+    mysql -uroot -p"${pass}" -e"set global validate_password_policy=0;" --connect-expired-password
+    mysql -uroot -p"${pass}" -e"set global validate_password_length=4;" --connect-expired-password
+    mysql -uroot -p"${pass}" -e"set global validate_password_mixed_case_count=0;" --connect-expired-password
+    mysql -uroot -p"${pass}" -e"set global validate_password_number_count=0;" --connect-expired-password
+    #echo 'enter your mysql password'
+    #read password
+    mysql -uroot -p"${pass}" -e"set password=password('${MYSQL_PASSWORD}');" --connect-expired-password
+    mysql -uroot -p"${MYSQL_PASSWORD}" -e"update mysql.user set host='%' where user='root';" --connect-expired-password
+    mysql -uroot -p"${MYSQL_PASSWORD}" -e"flush privileges;" --connect-expired-password
 
 }
 
 #安装mongodb,使用清华大学的源
 function install_mongodb() {
-echo "" > /etc/yum.repos.d/mongodb.repo
-for version in "3.0" "3.2" "3.4" "3.6" "4.0"; do
-cat >> /etc/yum.repos.d/mongodb.repo <<- EOF
+    echo "" > /etc/yum.repos.d/mongodb.repo
+    for version in "3.0" "3.2" "3.4" "3.6" "4.0"; do
+    cat >> /etc/yum.repos.d/mongodb.repo <<- EOF
 [mongodb-org-$version]
 name=MongoDB Repository
 baseurl=https://mirrors.tuna.tsinghua.edu.cn/mongodb/yum/el7-$version/
@@ -290,15 +293,15 @@ gpgcheck=0
 enabled=1
 
 EOF
-done
-yum makecache
-yum install mongodb-org -y
+    done
+    yum makecache
+    yum install mongodb-org -y
 
 }
 
 #安装docker,使用清华大学的源
 function install_docker() {
-cat > /etc/yum.repos.d/docker-ce.repo <<- "EOF"
+    cat > /etc/yum.repos.d/docker-ce.repo <<- "EOF"
 [docker-ce-stable]
 name=Docker CE Stable - $basearch
 baseurl=https://mirrors.aliyun.com/docker-ce/linux/centos/7/$basearch/stable
@@ -384,52 +387,55 @@ gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 
 EOF
-yum install docker-ce -y
-systemctl start docker
+    yum install docker-ce -y
+    systemctl start docker
 #配置国内docker加速器
 cat > /etc/docker/daemon.json <<- "EOF"
 {
   "registry-mirrors": ["https://registry.docker-cn.com"]
 }
 EOF
-systemctl restart docker
+    systemctl restart docker
 }
 system_config
 config_mirror_and_update
 install_usual_software
-if [[ -z $1 ]]; then
-install_python
-install_php
-install_nodejs_and_config
-install_cmd_game
-install_jdk_and_tomcat
-install_mysql_and_config
-install_mongodb
-install_docker
+if [[ -z $* ]]; then
+    install_python
+    install_php
+    install_nodejs_and_config
+    install_cmd_game
+    install_jdk_and_tomcat
+    install_mysql_and_config
+    install_mongodb
+    install_docker
 fi
-case $1 in
-python)
-install_python
-;;
-php)
-install_php
-;;
-nodejs)
-install_nodejs_and_config
-;;
-cmd_game)
-install_cmd_game
-;;
-jdk)
-install_jdk_and_tomcat
-;;
-mysql)
-install_mysql_and_config
-;;
-mongodb)
-install_mongodb
-;;
-docker)
-install_docker
-;;
-esac
+
+for arg in $* ; do
+    case $arg in
+    python)
+    install_python
+    ;;
+    php)
+    install_php
+    ;;
+    nodejs)
+    install_nodejs_and_config
+    ;;
+    cmd_game)
+    install_cmd_game
+    ;;
+    jdk)
+    install_jdk_and_tomcat
+    ;;
+    mysql)
+    install_mysql_and_config
+    ;;
+    mongodb)
+    install_mongodb
+    ;;
+    docker)
+    install_docker
+    ;;
+    esac
+done
